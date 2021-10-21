0c345650aa951a6396bda1b19e5f5292e1b436d9
Ralph Kube <ralph_kube@gmx.net>
2021-10-21 13:24:11 -0400

    Training loop for plain GAN

--- src/train_ecei.jl
+++ src/train_ecei.jl
@@ -3,13 +3,17 @@ using Flux
 using Flux.Data: DataLoader
 using Zygote
 using Printf
+using UnicodePlots
 
 
 n_features = 24*8
 latent_dim = 64
+batch_size = 512
+output_period = 100
+num_epochs = 1000
 
 data = get_framedata() |> gpu;
-train_loader = DataLoader(data, batchsize=64, shuffle=true)
+train_loader = DataLoader(data, batchsize=batch_size, shuffle=true)
 
 opt_dscr = ADAM(2e-4)
 opt_gen = ADAM(2e-4)
@@ -21,28 +25,44 @@ discriminator = Chain(Dense(n_features, 1024, x -> leakyrelu(x, 0.2f0)),
                       Dropout(0.3),
                       Dense(512, 256, x -> leakyrelu(x, 0.2f0)),
                       Dropout(0.3),
-                      Dense(256, 1, sigmoid)) |> gpu
+                      Dense(256, 1, sigmoid)) |> gpu;
 
 generator = Chain(Dense(latent_dim, 256, x -> leakyrelu(x, 0.2f0)),
                   Dense(256, 512, x -> leakyrelu(x, 0.2f0)),
                   Dense(512, 1024, x -> leakyrelu(x, 0.2f0)),
-                  Dense(1024, n_features, tanh)) |> gpu
+                  Dense(1024, n_features, tanh)) |> gpu;
 
 
+# Loss vectors
+lossvec_g = zeros(num_epochs)
+lossvec_d = zeros(num_epochs)
 
-# Flatten out training loop
-loss_sum_gen = 0.0f0
-loss_sum_dscr = 0.0f0
-x = first(train_loader)
-this_batch = size(x)[end]
-real_data = flatten(x)
+# Main training loop
+for n ∈ 1:num_epochs
+    Σ_loss_g = 0.0f0
+    Σ_loss_d = 0.0f0
 
-# Generate noise
-noise = randn(latent_dim, this_batch) |> gpu
-fake_data = generator(noise)
-loss_dscr = train_dscr!(discriminator, real_data, fake_data, this_batch, opt_dscr)
-loss_sum_dscr += loss_dscr
+    for x in train_loader
+        this_batch = size(x)[end]
+        real_data = flatten(x)
 
-loss_gen = train_gen!(discriminator, generator, opt_gen, latent_dim, batch_size)
-loss_sum_gen += loss_gen
+        # Generate noise
+        noise = randn(latent_dim, this_batch) |> gpu
+        fake_data = generator(noise)
+        Σ_loss_d += train_dscr!(discriminator, real_data, fake_data, this_batch, opt_dscr)
+        Σ_loss_g = train_gen!(discriminator, generator, opt_gen, latent_dim, batch_size)
+    end
+
+    lossvec_d[n] = Σ_loss_d / size(data)[end]
+    lossvec_g[n] = Σ_loss_g / size(data)[end]
+
+    if n % output_period == 0
+        @show n
+        noise = randn(latent_dim, 4) |> gpu;
+        fake_data = reshape(generator(noise), 24, 8*4);
+        p = heatmap(fake_data, colormap=:inferno)
+        print(p)
+    end 
+end
+
 
